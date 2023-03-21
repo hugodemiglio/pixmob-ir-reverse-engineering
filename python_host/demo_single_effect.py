eead4ae950082f4709daa69a365085a1ee0ac816
Hugo Demiglio <hugo@formaweb.com.br>
2023-03-20 23:40:31 -0300

    Serial address

--- python_host/demo_single_effect.py
+++ python_host/demo_single_effect.py
@@ -20,7 +20,7 @@ TAIL_CODE = "FADE_2"
 
 # Which serial port the Arduino is connected to. You can find this with the Arduino IDE or follow these instructions:
 # https://www.mathworks.com/help/supportpkg/arduinoio/ug/find-arduino-port-on-windows-mac-and-linux.html
-ARDUINO_SERIAL_PORT = "COM6"
+ARDUINO_SERIAL_PORT = "/dev/cu.usbmodem101"
 
 # Baud rate of the serial connection set up on the Arduino. It is 115200 in the included sketches.
 ARDUINO_BAUD_RATE = 115200
